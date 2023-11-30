59fab2f5daf95864e90e320bbb4d6fd15980fa6f
GRANT NATIONS <u1247784@umail.utah.edu>
2023-11-30 13:47:26 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,20 @@ export PYTHONPATH=$(pwd)
 # PROBLEM 2
 echo -e "\nRunning python script for problem 2...\n"
 python3 NeuralNetworks/predict_bank_notes.py
+
+# UNCOMMENT BELOW FOR HOMEWORK 4, IN CASE IT HAS NOT YET BEEN GRADED
+
+# #!/bin/bash
+
+# # set PYTHONPATH
+# export PYTHONPATH=$(pwd)
+
+# # PROBLEM 2
+# echo -e "\nRunning python script for problem 2...\n"
+# python3 SVM/PrimalSVM/predict_bank_notes.py
+
+# # PROBLEM 3
+# echo -e "\nRunning python script for problem 3...\n"
+# python3 SVM/DualSVM/predict_bank_notes.py
+
+# echo -e "\nDone.\n"
