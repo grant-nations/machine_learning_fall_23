f81eeea9c09467323cd57511354f8c3eb62a9f1c
GRANT NATIONS <u1247784@umail.utah.edu>
2023-11-29 18:27:22 -0700

    implement prediction script for nn

--- NeuralNetworks/nn.py
+++ NeuralNetworks/nn.py
@@ -24,9 +24,9 @@ class ThreeLayerNN:
         :param b3: The bias of the third layer.
         """
 
-        self.w1 = w1
+        self.w1 = w1  # (D, H1)
         self.w2 = w2  # (H1, H2)
-        self.w3 = w3
+        self.w3 = w3  # (H2,)
         self.b1 = b1
         self.b2 = b2
         self.b3 = b3
@@ -77,55 +77,58 @@ class ThreeLayerNN:
 
         # _____ layer 3 _____
 
-        dprediction = layer_3 - y  # (1,)
-        if verbose:
-            print(f"dprediction: {dprediction}")
-
-        dlayer_3 = dprediction * self.w3  # (1,) * (H2,) -> (H2,)
+        # dlayer_3 = dprediction * self.w3  # (1,) * (H2,) -> (H2,)
+        dlayer_3 = layer_3 - y # (1,)
         if verbose:
             print(f"dlayer_3: {dlayer_3}")
 
-        dw3 = dprediction * layer_2_sig  # (1,) * (H2,) -> (H2,)
+        # dw3 = dprediction * layer_2_sig  # (1,) * (H2,) -> (H2,)
+        dw3 = layer_2_sig * dlayer_3 # (H2,) * (1,) -> (H2,)
         if verbose:
             print(f"dw3: {dw3}")
-        db3 = dprediction  # (1,)
+
+        # db3 = dprediction  # (1,)
+        db3 = dlayer_3 # (1,)
         if verbose:
             print(f"db3: {db3}")
 
         # _____ layer 2 _____
 
-        dlayer_2_sig = dlayer_3 * layer_2_sig * (1 - layer_2_sig)  # (H2,) * (H2,) * (H2,) -> (H2,)
+        # dlayer_2_sig = dlayer_3 * layer_2_sig * (1 - layer_2_sig)  # (H2,) * (H2,) * (H2,) -> (H2,)
+        dlayer_2_sig = dlayer_3 * self.w3 # (1,) * (H2,) -> (H2,)
         if verbose:
             print(f"dlayer_2_sig: {dlayer_2_sig}")
 
-        dlayer_2 = dlayer_2_sig @ self.w2.T  # (H2,) @ (H2, H1) -> (H1,)
+        # dlayer_2 = dlayer_2_sig * self.w2.T  # (H2,) * (H2, H1) -> 
+        dlayer_2 = layer_2_sig * (1 - layer_2_sig) * dlayer_2_sig # (H2,) * (H2,) * (H2,) -> (H2,)
         if verbose:
             print(f"dlayer_2: {dlayer_2}")
 
-        dw2 = np.outer(layer_1_sig, dlayer_2_sig)  # (H1,) X (H2,) -> (H1, H2)
-
+        dw2 = np.outer(layer_1_sig, dlayer_2)  # (H1,) X (H2,) -> (H1, H2)
         if verbose:
             print(f"dw2: {dw2}")
 
-        db2 = dlayer_2_sig
+        db2 = dlayer_2
         if verbose:
             print(f"db2: {db2}")
 
         # _____ layer 1 _____
 
-        dlayer_1_sig = dlayer_2 * layer_1_sig * (1 - layer_1_sig)  # (H1,) * (H1,) * (H1,) -> (H1,)
+        # dlayer_1_sig = dlayer_2 * layer_1_sig * (1 - layer_1_sig)  # (H1,) * (H1,) * (H1,) -> (H1,)
+        dlayer_1_sig = dlayer_2 @ self.w2.T # (H2,) @ (H2, H1) -> (H1,)
         if verbose:
             print(f"dlayer_1_sig: {dlayer_1_sig}")
 
-        dlayer_1 = dlayer_1_sig @ self.w1.T  # (H1,) @ (H1, D) -> (D,)
+        # dlayer_1 = dlayer_1_sig @ self.w1.T  # (H1,) @ (H1, D) -> (D,)
+        dlayer_1 = layer_1_sig * (1 - layer_1_sig) * dlayer_1_sig # (H1,) * (H1,) * (H1,) -> (H1,)
         if verbose:
-            print(f"dlayer_1: {dlayer_1}")
+            print(f"dlayer_1: {dlayer_1_sig}")
 
         dw1 = np.outer(x, dlayer_1)  # (D,) X (H1,) -> (D, H1)
         if verbose:
             print(f"dw1: {dw1}")
 
-        db1 = dlayer_1_sig
+        db1 = dlayer_1
         if verbose:
             print(f"db1: {db1}")
 
--- NeuralNetworks/predict_bank_notes.py
+++ NeuralNetworks/predict_bank_notes.py
@@ -2,20 +2,21 @@ import os
 import pandas as pd
 import numpy as np
 from NeuralNetworks.nn import ThreeLayerNN
+import matplotlib.pyplot as plt
 
 data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Data', 'bank_note')
 train_data = pd.read_csv(os.path.join(data_dir, 'train.csv'), header=None)
 
 X = train_data.iloc[:, :-1]
 X = X.to_numpy()
-y = train_data.iloc[:, -1].replace(0, -1)
+y = train_data.iloc[:, -1]
 y = y.to_numpy()
 
 test_data = pd.read_csv(os.path.join(data_dir, 'test.csv'), header=None)
 
 X_test = test_data.iloc[:, :-1]
 X_test = X_test.to_numpy()
-y_test = test_data.iloc[:, -1].replace(0, -1)
+y_test = test_data.iloc[:, -1]
 y_test = y_test.to_numpy()
 
 
@@ -33,21 +34,82 @@ b3 = -1
 
 nn = ThreeLayerNN(w1, w2, w3, b1, b2, b3)
 
-x = np.array([1, 1])
-y = 1
+x_t = np.array([1, 1])
+y_t = 1
 
-nn.step(x, y, verbose=True)
+nn.step(x_t, y_t, verbose=True)
 
+# exit()
+print("------------- PART B ------------- \n")
 
-# print("------------- PART B ------------- \n")
+input_dim = len(X[0])
+hidden_dims = [5, 10, 25, 50, 100]
+epochs = 10
+gamma_0 = 0.1
+d = 1
 
-# input_dim = len(X[0])
-# hidden_dims = [5, 10, 25, 50, 100]
+for hidden_dim in hidden_dims:
+    print(f"Hidden dim: {hidden_dim}")
 
-# for hidden_dim in hidden_dims:
-#     w1 = np.random.randn(input_dim, hidden_dims[0])
-#     w2 = np.random.randn(hidden_dims[0], hidden_dims[1])
-#     w3 = np.random.randn(hidden_dims[1])  # the "1" dim is implied here
-#     b1 = np.random.randn()
-#     b2 = np.random.randn()
-#     b3 = np.random.randn()
+    w1 = np.random.randn(input_dim, hidden_dim)
+    w2 = np.random.randn(hidden_dim, hidden_dim)
+    w3 = np.random.randn(hidden_dim)  # the "1" dim is implied here
+    b1 = np.random.randn(hidden_dim)
+    b2 = np.random.randn(hidden_dim)
+    b3 = np.random.randn()
+
+    nn = ThreeLayerNN(w1, w2, w3, b1, b2, b3)
+    loss_history = nn.train(X, y, epochs, gamma_0, d)
+
+    # plt.plot(loss_history)
+    # plt.xlabel("Update")
+    # plt.ylabel("Loss")
+    # plt.title(f"Loss vs Update for hidden dim {hidden_dim}")
+    # plt.show()
+
+    incorrect_predictions = 0
+
+    for X_i, y_i in zip(X, y):
+        if nn.predict(X_i) != y_i:
+            incorrect_predictions += 1
+
+    print(f"Training error: {incorrect_predictions/len(X_test)}")
+
+    incorrect_predictions = 0
+
+    for X_i, y_i in zip(X_test, y_test):
+        if nn.predict(X_i) != y_i:
+            incorrect_predictions += 1
+
+    print(f"Testing error: {incorrect_predictions/len(X_test)}\n")
+
+print("------------- PART C ------------- \n")
+
+for hidden_dim in hidden_dims:
+    print(f"Hidden dim: {hidden_dim}")
+
+    w1 = np.zeros((input_dim, hidden_dim))
+    w2 = np.zeros((hidden_dim, hidden_dim))
+    w3 = np.zeros(hidden_dim)  # the "1" dim is implied here
+    b1 = np.zeros(hidden_dim)
+    b2 = np.zeros(hidden_dim)
+    b3 = 0
+
+    nn = ThreeLayerNN(w1, w2, w3, b1, b2, b3)
+    loss_history = nn.train(X, y, epochs, gamma_0, d)
+
+    incorrect_predictions = 0
+
+    for X_i, y_i in zip(X, y):
+        if nn.predict(X_i) != y_i:
+            incorrect_predictions += 1
+
+    print(f"Training error: {incorrect_predictions/len(X_test)}")
+
+    incorrect_predictions = 0
+
+    for X_i, y_i in zip(X_test, y_test):
+        if nn.predict(X_i) != y_i:
+            incorrect_predictions += 1
+
+    print(f"Testing error: {incorrect_predictions/len(X_test)}\n")
